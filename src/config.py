c9e41c65034fb76a56b35adbb798616a105edfa3
VortexOwl <glacialisMustela@proton.me>
2026-09-08 16:20:32 +0300

    Refactoring: 1. Bringing the project to PEP8 standards.

--- src/config.py
+++ src/config.py
@@ -14,10 +14,12 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 # Application code                                                            #
 # ----------------------------------------------------------------------------#
 
+
 class ServerConfig(BaseSettings):
     """
     Конфигурация uvicorn.
     """
+
     host: str = "127.0.0.1"
     port: int = 8000
     is_reload: bool = True
@@ -32,7 +34,7 @@ class ServerConfig(BaseSettings):
                 data["host"] = "0.0.0.0"
             if "is_reload" not in data:
                 data["is_reload"] = False
-                
+
         return data
 
 
@@ -40,20 +42,21 @@ class Config(BaseSettings):
     """
     Конфигурация для проведения анализа директории закладок браузера.
     """
-    model_config = SettingsConfigDict(env_prefix = "APP_")
+
+    model_config = SettingsConfigDict(env_prefix="APP_")
 
     log_level: int = 10
     is_open_webbrowser: bool = True
-    _default_profile_pattern: str = '*.default-default'
-    bookmarks_folder: str = 'KDE Store'
-    browser: str = 'Floorp'
-    browser_folder: str = '.floorp'
+    _default_profile_pattern: str = "*.default-default"
+    bookmarks_folder: str = "KDE Store"
+    browser: str = "Floorp"
+    browser_folder: str = ".floorp"
     browser_profile: str | None = None
     custom_report_file: str | None = None
-    database_file: str = 'places.sqlite'
-    data_folder: str = 'data'
-    report_folder: str = 'docs'
-    
+    database_file: str = "places.sqlite"
+    data_folder: str = "data"
+    report_folder: str = "docs"
+
     @property
     def path_data_folder(self) -> Path:
         return Path(self.data_folder) / self.database_file
@@ -61,13 +64,14 @@ class Config(BaseSettings):
     @property
     def report_file(self) -> str:
         return (
-            f'Bookmarks {self.bookmarks_folder}'
+            f"Bookmarks {self.bookmarks_folder}"
             if self.custom_report_file is None
             else self.custom_report_file
         )
+
     @property
     def is_docker(self) -> bool:
-        return Path('/.dockerenv').exists()
+        return Path("/.dockerenv").exists()
 
     @property
     def path_source_database(self) -> Path | None:
@@ -76,11 +80,11 @@ class Config(BaseSettings):
         path_browser: Path = Path(self.browser)
         path_browser_folder: Path = Path(self.browser_folder)
         path_profiles: Path
-        path_profile_bookmarks: Path        
+        path_profile_bookmarks: Path
 
-        if sys_name == 'Windows':
-            path_browser = Path('AppData') / 'Roaming' / path_browser / 'Profiles'
-        elif sys_name == 'Linux':
+        if sys_name == "Windows":
+            path_browser = Path("AppData") / "Roaming" / path_browser / "Profiles"
+        elif sys_name == "Linux":
             path_browser = path_browser_folder
         else:
             return None
@@ -91,11 +95,15 @@ class Config(BaseSettings):
             path_profile_bookmarks = path_profiles / self.browser_profile
         else:
             default_profile: Path = next(
-                (d for d in path_profiles.glob(self._default_profile_pattern) if d.is_dir()), 
+                (
+                    d
+                    for d in path_profiles.glob(self._default_profile_pattern)
+                    if d.is_dir()
+                ),
                 None,
             )
             if default_profile is None:
                 return None
             path_profile_bookmarks = default_profile
-    
+
         return path_profile_bookmarks / self.database_file
